180485baabc7337b15cfff5898f0d170dd1e0759
Karol Mondadori <karolmondadori@gmail.com>
2021-03-10 19:01:16 -0300

    update

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weather(props) {
-    function handleResponse(response) {
-        alert (`The weather in ${response.data.name} is ${response.data.main.temp}ºC`)
-        
+    const [city, setCity] = useState("");
+    const [message, setMessage] = useState(false);
+    const [weather, setWeather] = useState({});
+  
+    function displayWeather(response) {
+      setMessage(true);
+      setWeather({
+        temperature: response.data.main.temp,
+        wind: response.data.wind.speed,
+        humidity: response.data.main.humidity,
+        icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      });
     }
-    let apiKey = "994481b47d184794d0b8fe0506cdf444";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    
-    axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from Weather</h2>;
-}
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=994481b47d184794d0b8fe0506cdf444&units=metric`;
+      axios.get(apiUrl).then(displayWeather);
+    }
+  
+    function changeCity(event) {
+      setCity(event.target.value);
+    }
+  
+    let form = (
+      <form onSubmit={handleSubmit}>
+        <input className="enterCity" type="search" placeholder="City" onChange={changeCity} />
+        <button className="search" type="Submit"> Search </button>
+        <button className="current" type="Submit"> Current </button>
+      </form>
+    );
+  
+    if (message) {
+      return (
+        <div className="weather-app">
+          {form}
+          <ul>
+            <li className="temperature">☁️ Temperature: {Math.round(weather.temperature)}ºC</li>
+            <li className="humidity">💧 Humidity: {weather.humidity}%</li>
+            <li className="wind">🌬 Wind: {weather.wind}km/h</li>
+          </ul>
+          <h3>{city}</h3>
+          <img className="icon" src={weather.icon} alt="Weather Icon" />
+        </div>
+        
+      );
+    } else {
+      return form;
+    }
+  }
+  
